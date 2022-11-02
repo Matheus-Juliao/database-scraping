89336c0ac286b1489b4701f72c91c53ab0067715
Matheus Juliao <matheusjosejuliao@gmail.com>
2022-11-02 20:48:23 -0300

    create table cod_vehicle and adjust in database

--- scraping.sql
+++ scraping.sql
@@ -2,34 +2,64 @@ CREATE SCHEMA scraping;
 USE scraping;
 
 CREATE TABLE vehicle_table(
-id_vehicle_table VARCHAR(12) NOT NULL,
-brand VARCHAR(20),
-model VARCHAR(50),
-model_year VARCHAR(50),
-PRIMARY KEY (id_vehicle_table)
+	id_vehicle_table VARCHAR(12) NOT NULL,
+	brand VARCHAR(20),
+	model VARCHAR(50),
+	model_year VARCHAR(50),
+	PRIMARY KEY (id_vehicle_table)
 );
 
-create table query_table(
-id_query_table INT NOT NULL AUTO_INCREMENT,
-fipe_code VARCHAR(50),
-reference_month VARCHAR(50),
-authentication TEXT,
-consultation_date VARCHAR(50),
-average_price VARCHAR (20),
-id_vehicle_table VARCHAR(12) NOT NULL,
-PRIMARY KEY (id_query_table),
-FOREIGN KEY (id_vehicle_table) REFERENCES vehicle_table (id_vehicle_table)
+CREATE TABLE query_table(
+	id_query_table INT NOT NULL AUTO_INCREMENT,
+	fipe_code VARCHAR(50),
+	reference_month VARCHAR(50),
+	authentication TEXT,
+	consultation_date VARCHAR(50),
+	average_price VARCHAR (20),
+	fk_vehicle_table VARCHAR(12) NOT NULL,
+	PRIMARY KEY (id_query_table),
+	FOREIGN KEY (fk_vehicle_table) REFERENCES vehicle_table (id_vehicle_table)
+);
+
+CREATE TABLE cod_vehicle_table(
+	id_cod_vehicle_table INT NOT NULL AUTO_INCREMENT,
+	cod_brand VARCHAR(12) NOT NULL,
+	cod_model VARCHAR(12) NOT NULL ,
+	cod_model_year VARCHAR(12) NOT NULL ,
+	cod_reference_month VARCHAR(12) NOT NULL,
+    fk_cod_vehicle_table VARCHAR(12) NOT NULL,
+	PRIMARY KEY (id_cod_vehicle_table),
+	FOREIGN KEY (fk_cod_vehicle_table) REFERENCES vehicle_table (id_vehicle_table)
 );
 
 /*DROP SCHEMA fipe_scraping;
-DROP TABLE vehicle_table;
-DROP TABLE query_table;*/
+DROP TABLE cod_vehicle_table;
+DROP TABLE query_table;
+DROP TABLE vehicle_table;*/
 
-/*INSERT INTO vehicle_table(id_vehicle_table, brand, model, model_year) VALUES ("1", 'GM - Chevrolet','ONIX HATCH LT 1.4 8V FlexPower 5p Mec.', '2015 Gasolina');
-INSERT INTO query_table (fipe_code, reference_month, authentication, consultation_date, average_price,id_vehicle_table) VALUES ( "0940534" , 'outubro de 2022', 'wtktdjmyqmvt', 'sábado, 29 de outubro de 2022 12:38','R$ 46.247,00', "1");
+INSERT INTO vehicle_table(id_vehicle_table, brand, model, model_year) VALUES ("1", 'GM - Chevrolet','ONIX HATCH LT 1.4 8V FlexPower 5p Mec.', '2015 Gasolina');
+INSERT INTO query_table (fipe_code, reference_month, authentication, consultation_date, average_price, fk_vehicle_table) VALUES ( "0940534" , 'outubro de 2022', 'wtktdjmyqmvt', 'sábado, 29 de outubro de 2022 12:38','R$ 46.247,00', "1");
+INSERT INTO cod_vehicle_table (cod_brand, cod_model, cod_model_year, cod_reference_month, fk_cod_vehicle_table) VALUES ("23", "7691", "2019-1", "290", "1");
 
-select * from vehicle_table;
-select * from query_table;
+SELECT * FROM vehicle_table;
+SELECT * FROM query_table;
+SELECT * FROM cod_vehicle_table;
+	
+SELECT reference_month, 
+	fipe_code, brand, model, 
+    model_year, authentication, 
+    consultation_date  FROM vehicle_table 
+    INNER JOIN query_table ON query_table.fk_vehicle_table = "1";
+
+/*
+SELECT vehicle_table.brand, 
+	vehicle_table.model, 
+	vehicle_table.model_year, 
+	query_table.reference_month FROM vehicle_table 
+	INNER JOIN query_table ON query_table.id_vehicle_table = vehicle_table.id_vehicle_table
+	WHERE brand = "GM - Chevrolet" 
+	AND model = "ONIX HATCH Joy 1.0 8V Flex 5p Mec." 
+    AND model_year = "2019 Gasolina" AND query_table.reference_month = "outubro de 2022";
 
 create view join_tables as select id_vehicle_table, brand, model, model_year from vehicle_table;
 select * from join_tablessss;
